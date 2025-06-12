7839d8c00567599e3164b7fed34a030acf0c0d02
Rahul Kaul <kaulrahul9@gmail.com>
2025-06-12 10:46:03 +0200

    Update __main__.py

--- tap_mssql/__main__.py
+++ tap_mssql/__main__.py
@@ -4,4 +4,9 @@ from __future__ import annotations
 
 from tap_mssql.tap import Tapmssql
 
+if __name__ == "__main__":
+    # Create tap instance with parse_env_config=False
+    tap = Tapmssql(parse_env_config=False)
+    tap.cli()
+
 Tapmssql.cli()
